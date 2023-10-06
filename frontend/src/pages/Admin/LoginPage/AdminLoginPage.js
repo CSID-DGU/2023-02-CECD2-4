7846ded36cc3d7f168783b1ea8e615b3a4578ab0
zzz664 <zzz664@naver.com>
2023-10-06 15:56:58 +0900

    Admin Login Page 브랜치 추가

---
+++ frontend/src/pages/Admin/LoginPage/AdminLoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../../../components/Main/Main';
+
+const componentName = (props) => {
+    return (
+        <Main>
+            
+        </Main>
+    );
+};
+
+export default componentName;
