e71b5c19bc83839cac9ead943e755ad8349c4d07
zzz664 <zzz664@naver.com>
2023-10-06 17:07:01 +0900

    Admin Index Page 브랜치 생성

---
+++ frontend/src/pages/admin/AdminIndexPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../../components/Main/Main';
+
+const AdminIndexPage = (props) => {
+    return (
+        <Main>
+
+        </Main>
+    );
+};
+
+export default AdminIndexPage;
