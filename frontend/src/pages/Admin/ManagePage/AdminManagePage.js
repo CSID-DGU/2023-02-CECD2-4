a51b250aef0818eb41bc186b856a9c18ccfc853f
zzz664 <zzz664@naver.com>
2023-10-10 17:25:13 +0900

    admin manage page 완성

--- frontend/src/pages/Admin/ManagePage/AdminManagePage.js
+++ frontend/src/pages/Admin/ManagePage/AdminManagePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Main from '../../../components/Main/Main';
 import styled from 'styled-components';
 import KeywordSearchBox from './KeywordSearchBox';
-
+import KeywordList from './KeywordList';
 const AdminManagePage = (props) => {
     return (
         <Main>
             <KeywordSearchBox/>
+            <KeywordList/>
         </Main>
     );
 };
