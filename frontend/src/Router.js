b973669a3396a8de33f958d256001739e8155acf
zzz664 <zzz664@naver.com>
2023-10-10 14:16:55 +0900

    index page 구현 완료

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -6,6 +6,7 @@ import IndexPage from "./pages/IndexPage/IndexPage";
 import SearchResultPage from './pages/SearchResultPage/SearchResultPage';
 import EmotionDetailPage from './pages/EmotionDetailPage/EmotionDetailPage';
 import CommentDetailPage from './pages/CommentDetailPage/CommentDetailPage';
+import AdminIndexPage from './pages/Admin/IndexPage/AdminIndexPage';
 
 const Router = (props) => {
     return (
@@ -16,6 +17,7 @@ const Router = (props) => {
                     <Route path="/search_result/*" element={<SearchResultPage/>}></Route>
                     <Route path="/emotion_detail/*" element={<EmotionDetailPage/>}></Route>
                     <Route path="/comment_detail/*" element={<CommentDetailPage/>}></Route>
+                    <Route path="/admin/index/" element={<AdminIndexPage/>}></Route>
                 </Routes>
             <Footer/>
         </BrowserRouter>
--- frontend/src/pages/Admin/IndexPage/AdminIndexPage.js
+++ frontend/src/pages/Admin/IndexPage/AdminIndexPage.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Main from '../../components/Main/Main';
+import Main from '../../../components/Main/Main';
+import AddKeyword from './AddKeyword';
+import ManageKeyword from './ManageKeyword';
+import styled from 'styled-components';
+
+const ManagementContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+width:70%;
+height:60%;
+`;
 
 const AdminIndexPage = (props) => {
     return (
-        <Main>
-
+        <Main justify="center">
+            <ManagementContainer>
+                <AddKeyword/>
+                <ManageKeyword/>
+            </ManagementContainer>
         </Main>
     );
 };
