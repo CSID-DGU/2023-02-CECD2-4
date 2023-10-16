360b38ce0cbcb71618bcb5cb5303d792467b8f29
zzz664 <zzz664@naver.com>
2023-10-16 14:15:20 +0900

    react-redux 모듈 추가 및 redux설정 완료

---
+++ frontend/src/redux/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import toggleAdminHeader from './modules/toggleAdminHeader';
+
+const rootReducer = combineReducers({toggleAdminHeader});
+
+export default rootReducer;
---
+++ frontend/src/redux/modules/toggleAdminHeader.js
@@ -0,0 +1,20 @@
+const ENTER = 'toggleAdminHeader/ENTER';
+const LEAVE = 'toggleAdminHeader/LEAVE';
+
+export const enter = () => ({type:ENTER});
+export const leave = () => ({type:LEAVE});
+
+const isAdminInitialState = {isAdmin:false};
+
+const toggleAdminHeader = (state=isAdminInitialState, action) => {
+    switch(action.type) {
+        case ENTER:
+            return {isAdmin:!(state.isAdmin)};
+        case LEAVE:
+            return {isAdmin:!(state.isAdmin)};
+        default:
+            return state;
+    }
+};
+
+export default toggleAdminHeader;
