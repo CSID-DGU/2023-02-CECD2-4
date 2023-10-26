a1d98148769eb902de85503e9acbaf2fdc089450
blaxsior <heejun2798@gmail.com>
2023-10-26 19:37:46 +0900

    chore: docker-deploy.sh 파일 설정 수정

--- backend/docker-deploy.sh
+++ backend/docker-deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker build -t blaxsior/api-server -f ./server/Dockerfile ./server
+docker build -t blaxsior/api-server -f ./server/Dockerfile ./server'
+docker push blaxsior/api-server
