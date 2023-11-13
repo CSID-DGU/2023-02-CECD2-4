5b8f73baaa80b998729314c1493feae126d42616
KwangHo Cho <fishcopy6702@gmail.com>
2023-11-13 18:52:31 +0900

    feat: DummyDate DB 연결(저장)

--- ai-server-startkit/test2.py
+++ ai-server-startkit/test2.py
@@ -1,6 +1,7 @@
 # BERT model 테스트
 # 감정분석 + 원인분석 약 6분 소요(DummyData: 118개 뉴스기사의 118*5개 댓글)
 
+import datetime
 import time
 
 import sys
@@ -18,12 +19,13 @@ import pymysql
 from models import KcBERT, SBERT
 
 if __name__ == '__main__':
-    # DB init
+    ## DB init
     conn = pymysql.connect(host=keys['DB_HOST'], 
                            user=keys['DB_USER_NAME'], 
                            password=keys['DB_PASSWORD'], 
                            db='mydb', 
                            charset='utf8')
+    cursor = conn.cursor()
     
     # model init
     print("Model init....", end='')
@@ -33,8 +35,12 @@ if __name__ == '__main__':
     end_t = time.time()
     print("end! ({:0.5f} sec)".format(end_t - start_t))
     
-    # 결과 저장할 text파일
+    ## 결과 저장할 text파일
     text_file = open("result.txt", "w", encoding='utf-8')
+    Data_file = open("Data(2023.11.12).txt", "w", encoding='utf-8')
+    
+    # keyword 관리
+    keyword_dict = {"윤석열": 1, "이재명": 2}
     
     # sqs: SQSClient = boto3.client(
     #     'sqs',
@@ -54,11 +60,12 @@ if __name__ == '__main__':
     # sqs_response = getMessagesFromSQS(sqs, queue_url)
     # for res in sqs_response:
     #     news_response = getNewsResponse(s3, keys['S3_BUCKET'], res['key'])
-    #     print(news_response['keyword'])
-    #     print(news_response['data'][0]['comments'][0])
+    #     Data_file.write(str(news_response))
+    #     # print(news_response['keyword'])
+    #     # print(news_response['data'][0]['comments'][0])
     #     print(news_response)
     
-    # 하나의 받은 데이터 처리
+    ## 하나의 받은 데이터 처리
     print("Read data....", end='')
     start_t = time.time()
     with open('DummyData\\DummyData.json', 'rt', encoding='UTF8') as f:
@@ -67,6 +74,7 @@ if __name__ == '__main__':
     print("end! ({:0.5f} sec)".format(end_t - start_t))
 
     keyword = news_response['keyword']  # keyword(단어)
+    keyword_id = keyword_dict[keyword]  # keyword_id
     news_n = len(news_response['data']) # news 개수
     MIN_COMMENT = 5
     
@@ -80,27 +88,65 @@ if __name__ == '__main__':
             publishedDate = data['news']['publishedAt'] # 기사 발행 날짜 
             body = data['news']['body']     # 본문
             
-            # SBERT corpus init --> 빠른 속도 위함
+            ## SBERT corpus init --> 빠른 속도 위함
             SBERT_model.CorpusInit(corpus=body)
             
             for comment in comments:
-                comment_date = comment['date']          # 댓글 작성 날짜
-                comment_content = comment['contents']   # 댓글 내용
+                comment_date = comment['date']              # 댓글 작성 날짜
+                comment_content = comment['contents']       # 댓글 내용
+                comment_sympathy = comment['sympathyCount'] # 공감
+                comment_antipathy = comment['antipathyCount'] # 비공감
                 
                 emotion = KcBERT_model.predict(predict_sentence=comment_content)
                 related = SBERT_model.analysis(comments=[comment_content])
                 
-                ## 결과 출력해보고 싶다면 아래 주석 해제
+                # 결과 출력해보고 싶다면 아래 주석 해제
                 #
-                ## 감정 분석 결과
-                text_file.write("{} --> {}\n".format(comment_content, emotion))
+                # 감정 분석 결과
+                #text_file.write("{} --> {}\n".format(comment_content, emotion))
                 #print("{} --> {}".format(comment_content, emotion))
-                ## 연관도 분석 결과
-                text_file.write("{} --> {}\n\n".format(comment_content, related))
+                # 연관도 분석 결과
+                #text_file.write("{} --> {}\n\n".format(comment_content, related))
                 #print("{} --> {}".format(comment_content, related))
                 #print('')
+                
+                try:
+                    ## INSERT
+                    comment_date = datetime.datetime.strptime(comment_date, "%Y-%m-%dT%H:%M:%S%z")
+                    comment_date = str(comment_date.strftime("%Y-%m-%d %H:%M:%S"))
+                    
+                    insert_query = "INSERT INTO analysis_comment (createdAt, content, sympathy, antipathy, emotion, link, keyword_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    data_to_insert = (comment_date, comment_content, comment_sympathy, comment_antipathy, emotion, url, keyword_id)
+                    cursor.execute(insert_query, data_to_insert)
+                    
+                    lastrowid = cursor.lastrowid
+                    text = related['text']
+                    score = related['score']
+                    for i in range(0, len(text)):
+                        # print(text[i])
+                        # print(score[i])
+                        insert_query = "INSERT INTO article_content (content, score, comment_id) VALUES (%s, %s, %s)"
+                        data_to_insert = (text[i], score[i], lastrowid)
+                        cursor.execute(insert_query, data_to_insert)
+                    
+                except Exception as e:
+                    # fail
+                    conn.rollback()
+                    print("Error in INSERT")
+    
+    # commit
+    conn.commit()
+    
+    # comment_date = "2023-10-05T16:59:17+0900"
+    # comment_date = datetime.datetime.strptime(comment_date, "%Y-%m-%dT%H:%M:%S%z")
+    # comment_date = str(comment_date.strftime("%Y-%m-%d %H:%M:%S"))
+    # insert_query = "INSERT INTO analysis_comment (createdAt, content, sympathy, antipathy, emotion, link, keyword_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    # data_to_insert = (comment_date, "ddd", 123, 2, '혐오', 'www.naver.com', 1)
+    # cursor.execute(insert_query, data_to_insert)
+    # conn.commit()    
+    
     end_t = time.time()
     print("end! ({:0.5f} sec)".format(end_t - start_t))
-
     
-    
+    cursor.close()
+    conn.close()
